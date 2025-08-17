97cc39acdccdd3f105885d101cfee6b0ab496e45
anelamade@gmail.com <anelamade@gmail.com>
2025-08-17 11:47:06 -0700

    Discovering Databases: An Easy Start for Beginners

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
--- task_5.sql
-
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- Inserts a single row into customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
